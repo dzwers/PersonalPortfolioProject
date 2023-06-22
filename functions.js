36614e310251488e27c98f52dda26cb4aa97c244
David <davidzwers@gmail.com>
2023-06-22 03:56:12 -0400

    Add Contact Page

---
+++ functions.js
@@ -0,0 +1,7 @@
+//JavaScript code here
+
+function messagePrint () {
+    let message = document.getElementById('messageBox').value;
+    alert(message);
+    document.getElementById("messageBox").value = ''
+}
